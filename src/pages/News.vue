b0d81da9b69ef0092d3fe9d88cb68adb81f19ebd
barrystone <barrystone3181@gmail.com>
2021-01-20 11:18:06 +0800

    [Merge] merge Agenda.vue agenda.scss, add relative img

--- src/pages/News.vue
+++ src/pages/News.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="news">
-      <NewsHeader id="news-header" />
-      <Schedule id="news-schedule"/>
-      <Agenda id="news-agenda"/>
+    <NewsHeader id="news-header" />
+    <Schedule id="news-schedule" />
+    <Agenda id="news-agenda" />
     <!-- <Footer></Footer> should be put at App.vue (?) -->
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import NewsHeader from '@/components/news/Header.vue';
-import Schedule from '@/components/news/Schedule.vue';
-import Agenda from '@/components/news/Agenda.vue';
+import { Component, Prop, Vue } from "vue-property-decorator";
+import NewsHeader from "@/components/news/Header.vue";
+import Schedule from "@/components/news/Schedule.vue";
+import Agenda from "@/components/news/Agenda.vue";
 
 @Component({
   components: {
     NewsHeader,
     Schedule,
-    Agenda
-  }
+    Agenda,
+  },
 })
 export default class News extends Vue {
   @Prop() private msg!: string;
