74098e3c725289a58a41604385638aceb92443f2
barrystone <barrystone3183@gmail.com>
2021-03-24 21:50:02 +0800

    [Update] apply header in app.vue, and update home

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,17 +3,23 @@
   <div id="topHeader" class="header header--sticky">
     <div class="header__left">
       <div class="header-logo">
-        sitcon21
+        <a href="/2021/">
+          <img
+            class="sitcon-apple"
+            src="~@/assets/images/home/home-headerIcon.svg"
+            height="35"
+          />
+        </a>
       </div>
     </div>
     <div class="header__right">
       <div class="header-link">
         <ul>
-          <li><a href="">議程與活動</a></li>
-          <li><a href="">交通方式</a></li>
-          <li><a href="">會場地圖</a></li>
-          <li><a href="">贊助資訊</a></li>
-          <li><a href="">籌備團隊</a></li>
+          <li><a href="/2021/agenda">議程與活動</a></li>
+          <li><a href="/2021/traffic">交通方式</a></li>
+          <li><a href="/2021/venue">會場地圖</a></li>
+          <li><a href="/2021/sponsor">贊助資訊</a></li>
+          <li><a href="/2021/team">籌備團隊</a></li>
         </ul>
       </div>
       <div class="header-apply">
@@ -32,35 +38,29 @@ import { Component, Vue } from "vue-property-decorator";
 export default class Header extends Vue {
   //   header = document.getElementById("topHeader");
   //   sticky = this.header.offsetTop;
-  header = null;
-  sticky = null;
-
-  created() {
-    window.addEventListener("scroll", this.handleScroll);
-  }
-
-  destroyed() {
-    window.removeEventListener("scroll", this.handleScroll);
-  }
-
-  mounted() {
-    this.header = document.getElementById("topHeader");
-
-    this.sticky = this.header.offsetTop;
-    console.log("window.pageYOffset", window.pageYOffset);
-    console.log("sticky", this.sticky);
-  }
-
-  handleScroll(event) {
-    if (window.pageYOffset < this.sticky) {
-      this.header.classList.add("header--sticky");
-    } else {
-      this.header.classList.remove("header--sticky");
-    }
-
-    console.log("func window.pageYOffset", window.pageYOffset);
-    console.log("func sticky", this.sticky);
-  }
+  //   header = null;
+  //   sticky = null;
+  //   created() {
+  //     window.addEventListener("scroll", this.handleScroll);
+  //   }
+  //   destroyed() {
+  //     window.removeEventListener("scroll", this.handleScroll);
+  //   }
+  //   mounted() {
+  //     this.header = document.getElementById("topHeader");
+  //     this.sticky = this.header.offsetTop;
+  //     console.log("window.pageYOffset", window.pageYOffset);
+  //     console.log("sticky", this.sticky);
+  //   }
+  //   handleScroll(event) {
+  //     if (window.pageYOffset < this.sticky) {
+  //       this.header.classList.add("header--sticky");
+  //     } else {
+  //       this.header.classList.remove("header--sticky");
+  //     }
+  //     console.log("func window.pageYOffset", window.pageYOffset);
+  //     console.log("func sticky", this.sticky);
+  //   }
 }
 </script>
 
