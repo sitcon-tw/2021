8955460fa532296cca8c86868f8f8da0cceff54d
Tony Yang <t510599@gmail.com>
2021-03-25 15:09:48 +0800

    [Component] add EventBlock for Agenda

---
+++ src/components/EventBlock.vue
@@ -0,0 +1,27 @@
+<template>
+    <a href="" class="event block">
+        <img class="background" src="../assets/images/event-block-bg.svg">
+        <div class="inner">
+            <i v-if="icon" :class="icon" class="icon" />
+            <span>{{ newlineText }}</span>
+        </div>
+    </a>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({
+})
+export default class EventBlock extends Vue {
+    @Prop({ default: '' }) private text!: string;
+    @Prop({ default: '' }) private icon!: string;
+
+    get newlineText () {
+        return this.text.replace('\\n', '\n');
+    }
+}
+</script>
+<style lang="scss">
+@import '@/assets/scss/eventblock';
+</style>
--- src/pages/Agenda.vue
+++ src/pages/Agenda.vue
@@ -1,11 +1,30 @@
 <template>
+  <div id="agenda">
+    <div class="event container">
+      <img class="arrow" src="@/assets/images/arrow-left.svg">
+      <EventBlock icon="union" text="大地遊戲"/>
+      <EventBlock icon="stall-outline" text="社群攤位"/>
+      <EventBlock icon="group" text="天使計劃"/>
+      <EventBlock icon="lightning-outline" text="Lightning\nTalk" style="font-size: 20px"/>
+      <EventBlock icon="guide-outline" text="導遊團"/>
+      <EventBlock text="開放式\n議程"/>
+      <img class="arrow" src="@/assets/images/arrow-right.svg">
+    </div>
+  </div>
 </template>
-<script>
-import { Component, Prop, Vue } from 'vue-property-decorator';
+<script lang="ts">
+import { Watch, Component, Prop, Vue } from 'vue-property-decorator';
 
+import EventBlock from '../components/EventBlock.vue';
 @Component({
+  components: {
+    EventBlock
+  }
 })
 export default class Agenda extends Vue {
 
 }
 </script>
+<style lang="scss">
+@import '@/assets/scss/agenda';
+</style>
