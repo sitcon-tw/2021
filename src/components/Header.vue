fedf25217cecb199958bdada1813475d69103ee2
barrystone <barrystone3183@gmail.com>
2021-04-02 18:42:32 +0800

    [Update] update left content style in applypopup

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -87,7 +87,7 @@
       <div class="header-applyPopup-container">
         <div class="header-applyPopup__close">
           <a href="">
-            <img src="~@/assets/images/home/home-icon-close.svg" width="50" />
+            <img src="~@/assets/images/home/home-icon-close.svg" />
           </a>
         </div>
         <div class="header-applyPopup__title">
@@ -103,12 +103,17 @@
             <p>
               SITCON年會堅持不收費原則，秉持著「降低學生參與門檻」的理念，預算全數由個人／企業贊助或是政府／教育單位協助。
             </p>
+            <br />
+            <br />
             <p>
               若你認同 SITCON 的理念與做法，歡迎參與 SITCON 2021
               的個人贊助方案，給予我們實質上的支持。
               參與方案者將會獲贈入場資格與紀念品等項目，以表感謝。
             </p>
-            <p>*詳情請見個人贊助頁面</p>
+            <br />
+            <p class="header-applyPopup__content-left--noticeInfo">
+              *詳情請見個人贊助頁面
+            </p>
           </div>
           <div class="header-applyPopup__content-right">
             <div class="header-applyPopup-box">
