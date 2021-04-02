bb41e837ab7c0080d57a2b3f74d640f45572d5d8
Tony Yang <t510599@gmail.com>
2021-04-02 19:15:48 +0800

    [Update] use <router-link> in navbar using <a> to navigate between routes takes long time to load.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,76 +2,78 @@
   <div class="header header--sticky">
     <div class="header__left">
       <div class="header-logo">
-        <a href="/2021/">
+        <router-link to="/">
           <img src="~@/assets/images/home/home-headerIcon.svg" width="100%" />
-        </a>
+        </router-link>
       </div>
     </div>
     <div class="header__right">
       <div class="header-links">
         <ul>
           <li>
-            <a
-              href="/2021/agenda"
-              v-bind:class="onRoute('Agenda') ? 'header-link--active' : ''"
-              >議程與活動</a
+            <router-link
+              to="/agenda"
+              active-class="header-link--active"
+              >議程與活動</router-link
             >
           </li>
           <li>
-            <a
-              href="/2021/traffic"
-              v-bind:class="onRoute('Traffic') ? 'header-link--active' : ''"
-              >交通方式</a
+            <router-link
+              to="/traffic"
+              active-class="header-link--active"
+              >交通方式</router-link
             >
           </li>
           <li>
-            <a
-              href="/2021/venue"
-              v-bind:class="onRoute('Venue') ? 'header-link--active' : ''"
-              >會場地圖</a
+            <router-link
+              to="/venue"
+              active-class="header-link--active"
+              >會場地圖</router-link
             >
           </li>
           <li>
-            <a
-              href="/2021/sponsor"
-              v-bind:class="onRoute('Sponsor') ? 'header-link--active' : ''"
-              >贊助資訊</a
+            <router-link
+              to="/sponsor"
+              active-class="header-link--active"
+              >贊助資訊</router-link
             >
           </li>
           <li>
-            <a
-              href="/2021/team"
-              v-bind:class="onRoute('Team') ? 'header-link--active' : ''"
-              >籌備團隊</a
+            <router-link
+              to="/team"
+              active-class="header-link--active"
+              >籌備團隊</router-link
             >
           </li>
         </ul>
       </div>
 
       <div class="header-links--mobile">
-        <select onchange="location = this.value;">
-          <option value="/2021">年會主題</option>
+        <select @change="(ev) => { $router.push({ path: ev.target.value }); }">
+          <option value="/">年會主題</option>
           <option
-            value="/2021/agenda"
+            value="/agenda"
             :selected="onRoute('Agenda') ? true : false"
             >議程與活動</option
           >
           <option
-            value="/2021/traffic"
+            value="/traffic"
             :selected="onRoute('Traffic') ? true : false"
             >交通方式</option
           >
           <option
-            value="/2021/venue"
+            value="/venue"
             :selected="onRoute('Venue') ? true : false"
             >會場地圖</option
           >
           <option
-            value="/2021/sponsor"
+            value="/sponsor"
             :selected="onRoute('Sponsor') ? true : false"
             >贊助資訊</option
           >
-          <option value="/2021/team" :selected="onRoute('Team') ? true : false"
+          <option
+            value="/team"
+            :selected="onRoute('Team') ? true : false"
             >籌備團隊</option
           >
         </select>
