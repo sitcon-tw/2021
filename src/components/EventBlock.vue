3ab9937d9842a47f21051bb2dfb1356e99f5d041
Tony Yang <t510599@gmail.com>
2021-04-02 20:19:44 +0800

    [Update] make event block unclickable

--- src/components/EventBlock.vue
+++ src/components/EventBlock.vue
@@ -1,5 +1,5 @@
 <template>
-  <a href="" class="event block">
+  <a href="#" class="event block" :class="{ unclickable: !clickable }" @click.prevent="clickHandler">
     <img class="background" src="../assets/images/event-block-bg.svg">
     <div class="inner">
       <i v-if="icon" :class="icon" class="icon" />
@@ -17,9 +17,18 @@ export default class EventBlock extends Vue {
   @Prop({ default: '' }) private text!: string;
   @Prop({ default: '' }) private icon!: string;
   @Prop({ default: false }) private special!: boolean;
+  @Prop({ default: true }) private clickable!: boolean;
 
   get newlineText () {
     return this.text.replace('\\n', '\n');
+  }
+
+  private clickHandler (ev: Event) {
+    if (this.clickable) {
+      // route...?
+    }
+
+    return this.clickable;
   }
 }
 </script>
