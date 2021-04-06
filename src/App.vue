c725549da6d6ac8ce15676de4ce26f1708a675e0
Mirumo <mysteryperson520@gmail.com>
2021-04-06 18:04:29 +0800

    [fix] fix cfp bg

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <div id="app">
-    <Header v-if="isRoot()" v-bind:route="$route" />
-    <router-view />
-    <FooterPrimary v-if="isRoot()" v-bind:route="$route" />
-    <FooterSecondary v-if="!isRoot()" v-bind:route="$route" />
-  </div>
+	<div
+		id="app"
+		:class="
+			$route.name === 'CFP' ||
+			$route.name === 'ocfp-news' ||
+			$route.name === 'news'
+				? ''
+				: 'main-bg'
+		"
+	>
+		<Header v-if="isRoot()" v-bind:route="$route" />
+		<router-view />
+		<FooterPrimary v-if="isRoot()" v-bind:route="$route" />
+		<FooterSecondary v-if="!isRoot()" v-bind:route="$route" />
+	</div>
 </template>
 <script lang="ts">
 import { Component, Vue, Watch } from 'vue-property-decorator';
@@ -18,57 +27,57 @@ import FooterSecondary from '@/components/FooterSecondary.vue';
 import FooterPrimary from '@/components/FooterPrimary.vue';
 
 @Component({
-  components: {
-    Header,
-    FooterPrimary,
-    FooterSecondary
-  }
+	components: {
+		Header,
+		FooterPrimary,
+		FooterSecondary
+	}
 })
 export default class App extends Vue {
-  @Action('toggleDevice', { namespace: 'app' }) private toggleDevice!: (
-    device: DeviceType
-  ) => void;
-  @Getter('device', { namespace: 'app' }) private device!: DeviceType;
-  private deviceTypeMediaQuery: MediaQueryList = window.matchMedia(
-    '(min-width: 900px)'
-  );
+	@Action('toggleDevice', { namespace: 'app' }) private toggleDevice!: (
+		device: DeviceType
+	) => void;
+	@Getter('device', { namespace: 'app' }) private device!: DeviceType;
+	private deviceTypeMediaQuery: MediaQueryList = window.matchMedia(
+		'(min-width: 900px)'
+	);
 
-  public mounted() {
-    this.detectDeviceType(this.deviceTypeMediaQuery);
+	public mounted() {
+		this.detectDeviceType(this.deviceTypeMediaQuery);
 
-    // add change listener instead of resize listener
-    if (this.deviceTypeMediaQuery.addEventListener) {
-      this.deviceTypeMediaQuery.addEventListener(
-        'change',
-        this.detectDeviceType
-      );
-    } else {
-      // sad safari
-      this.deviceTypeMediaQuery.addListener(this.detectDeviceType);
-    }
-  }
+		// add change listener instead of resize listener
+		if (this.deviceTypeMediaQuery.addEventListener) {
+			this.deviceTypeMediaQuery.addEventListener(
+				'change',
+				this.detectDeviceType
+			);
+		} else {
+			// sad safari
+			this.deviceTypeMediaQuery.addListener(this.detectDeviceType);
+		}
+	}
 
-  public isRoot(): boolean {
-    if (
-      this.$route.name === 'CFP' ||
-      this.$route.name === 'news' ||
-      this.$route.name === 'ocfp-news'
-    ) {
-      return false;
-    } else {
-      return true;
-    }
-  }
+	public isRoot(): boolean {
+		if (
+			this.$route.name === 'CFP' ||
+			this.$route.name === 'news' ||
+			this.$route.name === 'ocfp-news'
+		) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 
-  private detectDeviceType(mq: MediaQueryList | MediaQueryListEvent): void {
-    const isDesktop: boolean = mq.matches;
+	private detectDeviceType(mq: MediaQueryList | MediaQueryListEvent): void {
+		const isDesktop: boolean = mq.matches;
 
-    if (isDesktop) {
-      this.toggleDevice(DeviceType.DESKTOP);
-    } else {
-      this.toggleDevice(DeviceType.MOBILE);
-    }
-  }
+		if (isDesktop) {
+			this.toggleDevice(DeviceType.DESKTOP);
+		} else {
+			this.toggleDevice(DeviceType.MOBILE);
+		}
+	}
 }
 </script>
 <style lang="scss">
