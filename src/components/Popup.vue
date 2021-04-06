696f35f4c314db7d0c0f8932bf650e07eaefd76f
barrystone <barrystone3183@gmail.com>
2021-04-07 01:58:29 +0800

    [Optimize] fix popup background scrolling problem

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,18 +1,38 @@
 <template>
-	<div class="popup" :id="id">
-		<div class="popup-bg" @click="() => $router.go(-1)"></div>
-		<slot></slot>
-	</div>
+  <div class="popup" :id="id">
+    <div
+      class="popup-bg"
+      @click="
+        () => {
+          $router.go(-1);
+          removeFixedScroll();
+        }
+      "
+    ></div>
+    <slot></slot>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Watch, Prop } from 'vue-property-decorator';
 
 @Component({
-	props: ['id']
+  props: ['id']
 })
 export default class Popup extends Vue {
-	@Prop() private id!: string;
+  @Prop() private id!: string;
+  public mounted() {
+    if (window.location.hash == `#${this.id}`) {
+      this.fixedScroll();
+    }
+  }
+
+  public fixedScroll() {
+    document.body.style.overflowY = 'hidden';
+  }
+  public removeFixedScroll() {
+    document.body.style.overflowY = 'scroll';
+  }
 }
 </script>
 <style lang="scss">
