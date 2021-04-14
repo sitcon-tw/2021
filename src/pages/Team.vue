80a9afe094d1d47a6053ca2b6374487128264d81
mysper <anthony.a3203@gmail.com>
2021-04-14 09:11:01 +0800

    [style] team style and add group img

--- src/pages/Team.vue
+++ src/pages/Team.vue
@@ -8,11 +8,11 @@
                 <p>{{group.intro}}</p>
                  <img
                     class="group-img"
-                    src="~@/assets/images/teams/dev.svg"
+                    :src="require(`../assets/images/teams/${getTeamImg(group.name)}.svg`)"
                 />
                 <div class="group-members">
                     <div v-for="member in group.member" class="group-member">
-                        <img :src="`https://www.gravatar.com/avatar/${member.mailHash}?s=320`" class="member-icon"/>
+                        <img :src="`https://www.gravatar.com/avatar/${member.mailHash}?s=320&d=https://sitcon.org/2021/img/404.svg&r=g`" class="member-icon"/>
                         <span class="group-member-text name">{{member.name}}</span>
                         <span class="group-member-text type">{{member.type}}</span>
                     </div>
@@ -35,6 +35,35 @@ import WIP from '@/components/WIP.vue';
 })
 export default class Team extends Vue {
     private team = team;
+
+    private getTeamImg (name: string): string {
+        switch (name) {
+            case '總召組':
+                return 'gen';
+            case '行政組':
+                return 'admin';
+            case '議程組':
+                return 'agenda';
+            case '議程助理':
+                return 'agenda';
+            case '行銷組':
+                return 'pr';
+            case '場務組':
+                return 'service';
+            case '文創組':
+                return 'design';
+            case '財務組':
+                return 'fin';
+            case '開發組':
+                return 'dev';
+            case '編輯組':
+                return 'edit';
+            case '攝影組':
+                return 'photo';
+            default:
+                return 'dev';
+        }
+    }
 }
 </script>
 <style lang="scss">
