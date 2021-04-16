2b72f1f47e58e12e8c7a942f1b923779180f8464
Mirumo <mysteryperson520@gmail.com>
2021-04-16 10:37:12 +0800

    [update] venue page and edit announcement

--- src/pages/Venue.vue
+++ src/pages/Venue.vue
@@ -1,15 +1,44 @@
 <template>
-    <WIP></WIP>
+    <article id="venue">
+        <section class="venue-title">
+            <img src="~@/assets/images/arrow-left-blue.svg"/>
+            <h1>
+                中研院-人文社會科學館
+                <br/>
+                場內地圖
+            </h1>
+            <img src="~@/assets/images/arrow-right-blue.svg"/>
+        </section>
+        <section class="venue-map">
+            <img class="floor" src="~@/assets/images/venue/3F-title.svg"/>
+            <img class="map" src="~@/assets/images/venue/3F/A.png"/>
+        </section>
+        <section class="venue-map venue-4f">
+            <img class="floor" src="~@/assets/images/venue/4F-title.svg"/>
+            <img class="map" src="~@/assets/images/venue/4F/A.png"/>
+        </section>
+        <BlockTitle text="攤位資訊"></BlockTitle>
+        <div class="temp">
+            <img 
+                src="~@/assets/images/teams/service.svg"
+            />
+            <h1>敬請期待...</h1>
+        </div>
+    </article>
 </template>
-<script>
+<script labg="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
-import WIP from '@/components/WIP.vue';
+import BlockTitle from '@/components/BlockTitle.vue';
 
 @Component({
     components: {
-        WIP
+        BlockTitle
     }
 })
 export default class Venue extends Vue {}
 </script>
+
+<style lang="scss">
+@import '@/assets/scss/venue';
+</style>
