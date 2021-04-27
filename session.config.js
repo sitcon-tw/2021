b272c0b05883ee61a1e932779ab775fa499758da
Denny Huang <denny0223@gmail.com>
2021-04-27 15:21:53 +0800

    [Update] session config avatar settings

--- session.config.js
+++ session.config.js
@@ -2,7 +2,7 @@ var config = {}
 
 config.spreadsheetKey = "15UCnp12IZEteM3b_wLT9_z4LcVWE65uMslGDkZP0FQs"
 
-config.default_avatar = "https://sitcon.org/2018/static/img/staffs/stone.png"
-config.avatar_base_url = "https://sitcon.org/2018/static/img/speaker/"
+config.default_avatar = "https://sitcon.org/2021/img/404.png"
+config.avatar_base_url = "https://sitcon.org/2021/img/speakers/"
 
 module.exports = config
