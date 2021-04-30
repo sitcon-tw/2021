65ccd7b8c53975634d55491a35e81672e3a6502d
Tony Yang <t510599@gmail.com>
2021-04-30 16:26:02 +0800

    [Update] event popup structure Add title and description Add close pop up function Update event data structure Reindent the file

--- src/components/EventPopUp.vue
+++ src/components/EventPopUp.vue
@@ -1,9 +1,21 @@
 <template>
-    <div class="main container">
-        {{ event.name }}
-        {{ event.icon }}
-        {{ eventData[event.name] }}
+  <div class="main container" :class="event.icon">
+    <div class="close button container">
+      <img
+        src="~@/assets/images/home/home-icon-close.svg"
+        @click="
+          () => {
+            removeFixed();
+            $emit('popup:close');
+          }
+        "
+      />
     </div>
+    <h1 class="title">{{ eventData[event.name].title }}</h1>
+    <div class="description">
+      {{ eventData[event.name].description }}
+    </div>
+  </div>
 </template>
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
@@ -13,9 +25,13 @@ import EventData from '@/../public/json/event.json';
 
 @Component({})
 export default class EventPopUp extends Vue {
-    @Prop() private event!: ConfEvent;
+  @Prop() private event!: ConfEvent;
 
-    private eventData = EventData;
+  private eventData = EventData;
+
+  private removeFixed () {
+    document.body.style.overflowY = 'scroll';
+  }
 }
 </script>
 <style lang="scss">
