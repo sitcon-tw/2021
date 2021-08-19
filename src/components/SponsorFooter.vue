dba795790a30088aebca505fd1ce35d6b88cd0c9
Mirumodapon <mysteryperson520@gmail.com>
2021-08-19 20:52:54 +0800

    [update] footer sponsor

--- src/components/SponsorFooter.vue
+++ src/components/SponsorFooter.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="sponsor-footer">
         <img v-for="x in coOrg" :src="require(`../assets/images/sponsor/${x.logo}`)"/>
+        <img v-for="x in supOrg" :src="require(`../assets/images/sponsor/${x.logo}`)"/>
         <template v-for="x in sponsors">
             <img v-for="i in x.org" :src="require(`../assets/images/sponsor/${i.logo}`)"/>
         </template>
@@ -16,6 +17,7 @@ import sponsorInfo from '../../template/sponsor.json';
 export default class SponsorFooter extends Vue {
   private coOrg = sponsorInfo.coOrg;
   private sponsors = sponsorInfo.sponsors;
+  private supOrg = sponsorInfo.supOrg;
 }
 </script>
 
