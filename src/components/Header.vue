cb624ea49ff6a477b840f06b212c701ef0c3e237
Mirumodapon <mysteryperson520@gmail.com>
2021-09-02 03:46:18 +0800

    [update] youtube link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -107,7 +107,7 @@
               今年 SITCON 將在 09/04 採線上進行，我們會透過 Youtube 直播議程，並透過 Gather 平台搭建虛擬年會會場，希望帶給大家有別於實體年會的有趣體驗。 點擊下方連結即可前往相關活動會場。
             </section>
             <section class="image-link">
-              <a>
+              <a href="https://www.youtube.com/user/SITCONArchive?sub_confirmation=1">
                 <img src="~@/assets/images/venue/apply-popup-yt.png"/>
                 <div class="filter"></div>
                 <div class="info">
